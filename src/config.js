6fe07b321791a0cf90ec819af7c368bc1cbf1956
Luis Laos <llaos.dev@gmail.com>
2025-02-11 05:55:57 -0500

    llaves publicas y privadas

--- src/config.js
+++ src/config.js
@@ -2,5 +2,7 @@ export default {
     // SECRET : 'secret_key_word',
     SECRET: process.env.JWT_SECRET || 'secreto_super_seguro',
     MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://admin:admin@hackaton17.crbbn.mongodb.net/H17',
-    CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173'
+    CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
+    PRIVATEKEY:process.env.PRIVATEKEY||'sk_test_SWyklAB8rIyjXmje',
+    PUBLICKEY:process.env.PUBLICKEY||'pk_test_89a1417406ce7fa2'
 }
