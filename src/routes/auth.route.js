82ed090cdfd7c2bc7ccc2603e19a8e880d27f5c2
Luis Laos <llaos.dev@gmail.com>
2025-02-14 01:42:31 -0500

    correcion ruta logout 2

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -8,6 +8,6 @@ import { verifySignup,authJwt } from "../middlewares";
 
 router.post('/signup',[verifySignup.checkUsernameExisted,verifySignup.checkEmailExisted,verifySignup.checkRoleExisted],authController.signUp);
 router.post('/signin',authController.signIn);
-router.post('/logout',authController.logout);
+router.get('/logout',authController.logout);
 
 export default router;
